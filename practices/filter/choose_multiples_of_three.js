672591ef427acc1a08e61ebf3ef035c1c3557ce8
Tommy Hui <aesops0124@gmail.com>
2018-11-21 14:18:19 +0800

    Updated Original Way for choose_multiple_of_three.js

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -6,8 +6,15 @@ function isMultipleOfThree(number) {
 
 function choose_multiples_of_three(collection) {
 
-	return collection.filter(number => isMultipleOfThree(number))
-  //implement here
+	let result = [];
+	for(let i=0; i<collection.length; i++) {
+		if(isMultipleOfThree(collection[i]))
+			result.push(collection[i]);
+	}
+	return result;
+	
+	//return collection.filter(number => isMultipleOfThree(number))
+
 }
 
 module.exports = choose_multiples_of_three;
