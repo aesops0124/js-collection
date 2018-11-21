deb5f95c1a693f1269ac4fdcf5186316880f5b98
Tommy Hui <aesops0124@gmail.com>
2018-11-21 14:17:11 +0800

    [Tommy] Updated Original Way for choose_even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,18 @@
 'use strict';
 
+function isEven(number) {
+	return (number % 2 === 0)
+}
 function choose_even(collection) {
 
-  //implement here
+	let result = [];
+	for(let i=0; i<collection.length; i++) {
+		if(isEven(collection[i]))
+			result.push(collection[i]);
+	}
+	return result;
+	//return collection.filter(number => isEven(number))
+
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,7 +1,12 @@
 'use strict';
 
+function isMultipleOfThree(number) {
+	return (number % 3 === 0)
+}
+
 function choose_multiples_of_three(collection) {
 
+	return collection.filter(number => isMultipleOfThree(number))
   //implement here
 }
 
