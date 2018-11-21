93cd574aa534b53857a2886057a30f67db9cebfa
Tommy Hui <aesops0124@gmail.com>
2018-11-21 14:48:44 +0800

    [Tommy] Improved Way for map

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,10 +1,5 @@
 'use strict';
 function map_to_even(collection){
-  let result = [];
-  	for(let i=0; i<collection.length; i++) {
-		result.push(collection[i]*2);
-	}
-	return result;
-
+  return collection.map(num => num * 2);
 }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,10 +1,6 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  let result = [];
-  	for(let i=0; i<collection.length; i++) {
-		result.push(collection[i]*4+1);
-	}
-	return result;
+  return collection.map(num => num * 4 + 1);
 
 }
 module.exports = map_to_four_multiples_add_one;
